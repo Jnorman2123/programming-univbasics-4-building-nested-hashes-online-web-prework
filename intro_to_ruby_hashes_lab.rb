58435b2421a61e7146be1ab8af414c73d8d0e6b2
Jonathan Norman <jnorman2123@gmail.com>
2019-09-30 19:40:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,3 +5,9 @@ end
 def monopoly_with_second_tier 
   monopoly = {railroads: {pieces: 4}}
 end 
+
+def monopoly_with_third_tier
+  monopoly = {
+    railroads: {pieces: 4, rent_in_dollars: "value"}
+  }
+end 
