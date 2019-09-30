eabc61d2c386e9f891811e881b9561e08f20ed7d
Jonathan Norman <jnorman2123@gmail.com>
2019-09-30 19:44:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,8 +9,9 @@ end
 def monopoly_with_third_tier
   monopoly = {
     railroads: {
-      pieces: 4, rent_in_dollars: {key1: "value1", key2: "value2", key3: "value3", key4: "value4"},
-      names: "value"
+      pieces: 4, 
+      rent_in_dollars: {key1: "value1", key2: "value2", key3: "value3", key4: "value4"},
+      names: {key1: "value1", key2: "value2", key3: "value3", key4: "value4"}
     }
   }
 end 
